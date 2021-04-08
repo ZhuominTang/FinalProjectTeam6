7e842e464e34550db439c757f680561da3f65d4b
ZhuominTang <tang.zhuo@northeastern.edu>
2021-04-08 11:35:14 +0800

    1.0

---
+++ FinalProject/db4odemo/src/Business/Employee/Employee.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Employee;
+
+import Business.Business.Person;
+
+
+/**
+ *
+ * @author raunak
+ */
+public class Employee extends Person{
+    
+   
+    private int id;
+    private static int count = 1;
+
+    public Employee() {
+       
+        id = count;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+  
+
+    @Override
+    public String toString() {
+        return this.returnFullName();
+    }
+    
+    
+}
---
+++ FinalProject/db4odemo/src/Business/Role/CustomerRole.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.Business.EcoSystem;
+
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+
+
+
+import Business.UserAccount.UserAccount;
+import javax.swing.JFrame;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author raunak
+ */
+public class CustomerRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem system,JFrame frame) {
+         return null;
+    }
+
+   
+    
+    
+}
